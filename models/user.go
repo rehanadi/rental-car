a89ff707e47ef467d2bc8843efc0b7f0b587a50d
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-12-18 16:54:28 +0700

    feat: add user model

---
+++ models/user.go
@@ -0,0 +1,25 @@
+package models
+
+type User struct {
+	UserID        int     `json:"user_id" gorm:"primaryKey"`
+	Name          string  `json:"name"`
+	Email         string  `json:"email"`
+	Password      string  `json:"password"`
+	DepositAmount float64 `json:"deposit_amount"`
+}
+
+type RegisterRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterResponse struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
