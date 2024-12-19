579a0dc42daf89a11b6367568f70a13e94f12fb8
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-12-19 16:17:24 +0700

    feat: add payment route

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"rental-car/config"
 	"rental-car/controllers"
+	m "rental-car/middlewares"
 	"rental-car/repositories"
 
 	"github.com/labstack/echo/v4"
@@ -16,4 +17,12 @@ func Init(e *echo.Echo) {
 	u := e.Group("/users")
 	u.POST("/register", userController.Register)
 	u.POST("/login", userController.Login)
+
+	// payment routes
+	paymentRepository := repositories.NewPaymentRepository(config.DB)
+	paymentController := controllers.NewPaymentController(paymentRepository)
+
+	p := e.Group("/payments")
+	p.POST("/top-up", m.Authentication(paymentController.TopUpDeposit))
+	p.GET("/verify/:id", m.Authentication(paymentController.VerifyPayment))
 }
