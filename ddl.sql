4fb28c553db41bad2c81471c49c41c3ff1d92f53
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-12-19 16:17:48 +0700

    db: add table payment

--- ddl.sql
+++ ddl.sql
@@ -6,3 +6,14 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   deposit_amount DECIMAL(10, 2) DEFAULT 0.00
 );
+
+-- Create payments table with status
+CREATE TABLE payments (
+  payment_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(user_id),
+  amount DECIMAL(10, 2) NOT NULL,
+  payment_method VARCHAR(100) NOT NULL,
+  status VARCHAR(100) DEFAULT 'pending',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
