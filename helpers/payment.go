a1ac242a052005d93ca36191732d005cfec77811
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-12-19 17:04:08 +0700

    feat: create xendit invoice helper

---
+++ helpers/payment.go
@@ -0,0 +1,57 @@
+package helpers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+	"rental-car/models"
+)
+
+func CreateInvoice(req models.CreateInvoiceRequest) (*models.CreateInvoiceResponse, int, error) {
+	apiURL := os.Getenv("XENDIT_API_URL")
+	apiKey := os.Getenv("XENDIT_SECRET_KEY")
+
+	body := map[string]interface{}{
+		"external_id":      req.ExternalId,
+		"amount":           req.Amount,
+		"description":      req.Description,
+		"invoice_duration": req.InvoiceDuration,
+		"customer": map[string]string{
+			"given_names": req.GivenNames,
+			"email":       req.Email,
+		},
+		"currency":             req.Currency,
+		"payment_methods":      []string{req.PaymentMethod},
+		"success_redirect_url": req.SuccessRedirectURL,
+		"failure_redirect_url": req.FailureRedirectURL,
+	}
+
+	reqBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	request.SetBasicAuth(apiKey, "")
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	defer response.Body.Close()
+
+	var resInvoice models.CreateInvoiceResponse
+	if err := json.NewDecoder(response.Body).Decode(&resInvoice); err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	return &resInvoice, response.StatusCode, nil
+}
