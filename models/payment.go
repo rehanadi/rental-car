a4ff35d85b1d37164f645b584e7902d6942a7b57
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2024-12-19 17:04:21 +0700

    feat: add invoice model

--- models/payment.go
+++ models/payment.go
@@ -25,3 +25,23 @@ type TopUpDepositResponse struct {
 	CreatedAt     string  `json:"created_at"`
 	RedirectURL   string  `json:"redirect_url"`
 }
+
+type CreateInvoiceRequest struct {
+	ExternalId         string  `json:"external_id"`
+	Amount             float64 `json:"amount"`
+	Description        string  `json:"description"`
+	InvoiceDuration    int     `json:"invoice_duration"`
+	GivenNames         string  `json:"given_names"`
+	Email              string  `json:"email"`
+	Currency           string  `json:"currency"`
+	PaymentMethod      string  `json:"payment_method"`
+	SuccessRedirectURL string  `json:"success_redirect_url"`
+	FailureRedirectURL string  `json:"failure_redirect_url"`
+}
+
+type CreateInvoiceResponse struct {
+	Id         string  `json:"id"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	InvoiceURL string  `json:"invoice_url"`
+}
